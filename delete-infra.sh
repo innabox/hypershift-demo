9ae8d414da94aaac5059a07af3d346c511f57c35
Lars Kellogg-Stedman <lars@redhat.com>
2025-04-16 14:35:21 -0400

    Make delete-infra.sh more robust
    Exit on legitimate errors, and ensure that we clean up  ports before
    attempting to delete the subnet.

--- delete-infra.sh
+++ delete-infra.sh
@@ -2,9 +2,12 @@
 
 clustername=$1
 
-set -x
+set -ex
 
-openstack router remove subnet "${clustername}-router" "${clustername}-subnet"
-openstack router delete "${clustername}-router"
-openstack subnet delete "${clustername}-subnet"
-openstack network delete "${clustername}-network"
+openstack router show "${clustername}-router" > /dev/null && {
+  openstack router remove subnet "${clustername}-router" "${clustername}-subnet"
+  openstack router delete "${clustername}-router"
+}
+openstack port list --network "${clustername}-network" -f value -c ID | xargs -n1 -r openstack port delete
+openstack subnet show "${clustername}-subnet" > /dev/null && openstack subnet delete "${clustername}-subnet"
+openstack network show "${clustername}-network" > /dev/null && openstack network delete "${clustername}-network"
